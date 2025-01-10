3bdcb9f60e6eeac00477ad34c60bd206440d5100
Almas3211 <bhattialmas77@gmail.com>
2025-01-10 09:59:19 +0000

    conta

--- prova.cpp
+++ prova.cpp
@@ -1,6 +1,17 @@
-#include <iostream> // Include la libreria per l'input/output
+int main()
+{int n,x,conta=0,max=0;
+ cout<< " quanti numeri vuoi inserire \n";
+    cin >> n;
+    while(n>conta){
+        cout<< " inserisci un numero \n";
+    cin >> x;
+    if(x>max){max=x;
+        
+    }
+    
+    conta=conta+1;
+    
+    }
+    cout<<"il numero più alto tra quelli inseriti è "<<max;
+    }
 
-int main() {
-    std::cout << "Ciaoa tutti" << std::endl; // Stampa "Ciao mondo"
-    return 0; // Termina il programma
-}
