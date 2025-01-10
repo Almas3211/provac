f640bb6df24ac3e8822672e41e8618398397d5fe
Almas3211 <bhattialmas77@gmail.com>
2025-01-10 09:57:23 +0000

    ciao mondo

---
+++ prova.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Include la libreria per l'input/output
+
+int main() {
+    std::cout << "Ciao mondo a tutti" << std::endl; // Stampa "Ciao mondo"
+    return 0; // Termina il programma
+}
